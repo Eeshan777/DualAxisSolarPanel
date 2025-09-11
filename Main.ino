7c7a0069bf417040e7834280980512f769861786
Eeshan777 <eeshanxxxx@gmail.com>
2025-09-11 13:50:06 +0530

    Final Commit

--- Main.ino
+++ Main.ino
@@ -1,42 +1,106 @@
 #include <Wire.h>
 #include <Adafruit_INA219.h>
+#include<Servo.h>
 
 Adafruit_INA219 ina219;
 
-const int mosfetGatePin = 7;
-const float cutoffVoltage = 4.10;
-const float resumeVoltage = 3.50;
-int chargingEnabled = 1;
+const int switching = 26;
+const float upper = 8.00;
+const float lower = 6.50;
+int charging = 1;
+
+Servo pan, tilt;
+int panPos=90;
+int tiltPos=90;
+float angle=1;
+
+float tolerance=200;
 
 void setup()
 {
   Serial.begin(9600);
   ina219.begin();
-  pinMode(mosfetGatePin, OUTPUT);
-  digitalWrite(mosfetGatePin, LOW);
-  chargingEnabled = 1;
+  pinMode(switching, OUTPUT);
+  digitalWrite(switching, LOW);
+  charging = 1;
 
-  Serial.println("Battery charging control started...");
+  Serial.println("Battery charging control started......");
+
+  pan.attach(18);
+  tilt.attach(19);
+  pan.write(90);
+  tilt.write(90);
+
+  Serial.println("Pan & Tilt Started......");
 }
 
-void loop() {
-  float busVoltage = ina219.getBusVoltage_V();
-  float current_mA = ina219.getCurrent_mA()/1000;
+void loop()
+{
+  BMS();
+}
+
+void BMS()
+{
+  float volt = ina219.getBusVoltage_V();
+  float current = ina219.getCurrent_mA()/1000;
+  float batteryPercent=(volt/100)*8.4;
 
   Serial.print("Battery Voltage: ");
-  Serial.print(busVoltage, 3);
-  Serial.print(" V | Current: ");
-  Serial.print(current_mA, 1);
-  Serial.println(" A");
+  Serial.print(volt, 3);
+  Serial.print("V  Current: ");
+  Serial.print(current, 1);
+  Serial.println("A");
+  Serial.print("Battery Percentage: ");
+  Serial.println("%");
 
-  if (busVoltage >= cutoffVoltage && chargingEnabled == 1)
+  if (volt >= upper && charging == 1)
   {
-    digitalWrite(mosfetGatePin, HIGH);
-    chargingEnabled = 0;
+    digitalWrite(switching, HIGH);
+    charging = 0;
   }
-  if(busVoltage <= resumeVoltage && chargingEnabled == 0)
+  if(volt <= lower && charging == 0)
   {
-    digitalWrite(mosfetGatePin, LOW);
-    chargingEnabled = 1;
+    digitalWrite(switching, LOW);
+    charging = 1;
+  }
+  if(volt>lower)
+  {
+    DualAxis();
+  }
+}
+
+void DualAxis()
+{
+  float ldrUp=analogRead(34);
+  float ldrDown=analogRead(35);
+  float ldrRight=analogRead(32);
+  float ldrLeft=analogRead(33);
+
+  if(abs(ldrLeft-ldrRight)>tolerance)
+  {
+    if(ldrLeft>ldrRight)
+    {
+      panPos-=angle;
+    }
+    else
+    {
+      panPos+=angle;
+    }
+    panPos = constrain(panPos, 0, 180);
+    pan.write(panPos);
+  }
+
+  if(abs(ldrUp-ldrDown)>tolerance)
+  {
+    if(ldrUp>ldrDown)
+    {
+      tiltPos+=angle;
+    }
+    else
+    {
+      tiltPos-=angle;
+    }
+    tiltPos = constrain(tiltPos, 0, 180);
+    tilt.write(tiltPos);
   }
 }
