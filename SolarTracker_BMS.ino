979628e21afcc27c1f8dde64c7e5e6aeab4c1a24
Eeshan777 <eeshanxxxx@gmail.com>
2026-03-07 01:04:06 +0530

    Final Commit

--- SolarTracker_BMS.ino
+++ SolarTracker_BMS.ino
@@ -1,106 +1,122 @@
 #include <Wire.h>
-#include <Adafruit_INA219.h>
-#include<Servo.h>
+#include "INA3221.h"
+#include <Servo.h>
 
-Adafruit_INA219 ina219;
+INA3221 ina[2]=
+{
+	INA3221(0x40,&Wire),
+	INA3221(0x41,&Wire)
+};
 
-const int switching = 26;
-const float upper = 8.00;
-const float lower = 6.50;
-int charging = 1;
+const int switching=26;
+const float upper=25.2;
+const float lower=19.5;
+int charging=1;
 
-Servo pan, tilt;
+Servo pan,tilt;
 int panPos=90;
 int tiltPos=90;
-float angle=1;
 
+float angle=1;
 float tolerance=200;
+float cellVoltage[6];
+float cellCurrent[6];
 
 void setup()
 {
-  Serial.begin(9600);
-  ina219.begin();
-  pinMode(switching, OUTPUT);
-  digitalWrite(switching, LOW);
-  charging = 1;
+	Serial.begin(115200);
+	Wire.begin();
+	Wire.setClock(400000);
 
-  Serial.println("Battery charging control started......");
+	for(int i=0;i<2;i++)
+	{
+		ina[i].begin();
+		ina[i].setShuntR(0,0.1);
+		ina[i].setShuntR(1,0.1);
+		ina[i].setShuntR(2,0.1);
+	}
 
-  pan.attach(18);
-  tilt.attach(19);
-  pan.write(90);
-  tilt.write(90);
-
-  Serial.println("Pan & Tilt Started......");
+	pinMode(switching,OUTPUT);
+	digitalWrite(switching,LOW);
+	pan.attach(18);
+	tilt.attach(19);
+	pan.write(90);
+	tilt.write(90);
+	Serial.println("6 Cell BMS + Solar Tracker Started");
 }
 
 void loop()
 {
-  BMS();
-}
+	int index=0;
+	float packVoltage=0;
+	for(int i=0;i<2;i++)
+	{
+		for(int cell=0;cell<3;cell++)
+		{
+			float volt=ina[i].getBusVoltage(cell);
+			float current=ina[i].getCurrent_mA(cell)/1000.0;
+			cellVoltage[index]=volt;
+			cellCurrent[index]=current;
+			packVoltage+=volt;
 
-void BMS()
-{
-  float volt = ina219.getBusVoltage_V();
-  float current = ina219.getCurrent_mA()/1000;
-  float batteryPercent=(volt/100)*8.4;
+			Serial.print("Cell ");
+			Serial.print(index+1);
+			Serial.print(": ");
+			Serial.print(volt,3);
+			Serial.print(" V  ");
+			Serial.print(current,3);
+			Serial.println(" A");
+			index++;
+		}
+	}
+	float batteryPercent=(packVoltage/25.2)*100;
+	Serial.print("Pack Voltage: ");
+	Serial.print(packVoltage,3);
+	Serial.println(" V");
+	Serial.print("Battery Percentage: ");
+	Serial.print(batteryPercent);
+	Serial.println(" %");
+	Serial.println();
 
-  Serial.print("Battery Voltage: ");
-  Serial.print(volt, 3);
-  Serial.print("V  Current: ");
-  Serial.print(current, 1);
-  Serial.println("A");
-  Serial.print("Battery Percentage: ");
-  Serial.println("%");
-
-  if (volt >= upper && charging == 1)
-  {
-    digitalWrite(switching, HIGH);
-    charging = 0;
-  }
-  if(volt <= lower && charging == 0)
-  {
-    digitalWrite(switching, LOW);
-    charging = 1;
-  }
-  if(volt>lower)
-  {
-    DualAxis();
-  }
+	if(packVoltage>=upper&&charging==1)
+	{
+		digitalWrite(switching,HIGH);
+		charging=0;
+	}
+	if(packVoltage<=lower&&charging==0)
+	{
+		digitalWrite(switching,LOW);
+		charging=1;
+	}
+	if(packVoltage>lower)
+	{
+		DualAxis();
+	}
+	delay(100);
 }
 
 void DualAxis()
 {
-  float ldrUp=analogRead(34);
-  float ldrDown=analogRead(35);
-  float ldrRight=analogRead(32);
-  float ldrLeft=analogRead(33);
-
-  if(abs(ldrLeft-ldrRight)>tolerance)
-  {
-    if(ldrLeft>ldrRight)
-    {
-      panPos-=angle;
-    }
-    else
-    {
-      panPos+=angle;
-    }
-    panPos = constrain(panPos, 0, 180);
-    pan.write(panPos);
-  }
-
-  if(abs(ldrUp-ldrDown)>tolerance)
-  {
-    if(ldrUp>ldrDown)
-    {
-      tiltPos+=angle;
-    }
-    else
-    {
-      tiltPos-=angle;
-    }
-    tiltPos = constrain(tiltPos, 0, 180);
-    tilt.write(tiltPos);
-  }
+	float ldrUp=analogRead(34);
+	float ldrDown=analogRead(35);
+	float ldrRight=analogRead(32);
+	float ldrLeft=analogRead(33);
+	if(abs(ldrLeft-ldrRight)>tolerance)
+	{
+		if(ldrLeft>ldrRight)
+			panPos-=angle;
+		else
+			panPos+=angle;
+		panPos=constrain(panPos,0,180);
+		pan.write(panPos);
+	}
+	if(abs(ldrUp-ldrDown)>tolerance)
+	{
+		if(ldrUp>ldrDown)
+			tiltPos+=angle;
+		else
+			tiltPos-=angle;
+		tiltPos=constrain(tiltPos,0,180);
+		tilt.write(tiltPos);
+	}
 }
